9c20111a23d281b6884f4cb7bab7e0e684d8db25
Maysara Safwat <maisarasafwat2@gmail.com>
2023-03-14 15:31:11 +0200

    complete lab3

--- day3/views/counter.php
+++ day3/views/counter.php
@@ -3,4 +3,4 @@
 $counter = new Counter();
 $counter->increment_and_update();
 $count = $counter->get_count();
-echo "<h1> count : ".$count."</h1>";
+echo "<center> <h1 style='color: #1c9288; margin: 10px auto;'> Count : ".$count."</h1> </center>";
